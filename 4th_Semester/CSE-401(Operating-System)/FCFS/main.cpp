a1c568d211bcdf332b89ea809e0b0f8cf78e3137
Rakibul1411 <bsse1411@iit.du.ac.bd>
2024-04-01 05:17:21 +0600

    update

--- 4th_Semester/CSE-401(Operating-System)/FCFS/main.cpp
+++ 4th_Semester/CSE-401(Operating-System)/FCFS/main.cpp
@@ -2,82 +2,94 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-const int p=5;
 struct Process {
     string name;
-    int burst_time;
+    int burst_time{};
     int arrival_time=0;
     int completion_time=0;
     int turn_around_time=0;
     int waiting_time=0;
-}process[p];
+};
 
 int main() {
+    int p;
+    cout<<"Enter the number of process: ";
+    cin>> p;
 
-    for (int i = 0; i < p; ++i) {
-        cout<<"Input Process name: ";
-        cin >> process[i].name;
-        cout<<"Input Brust time: ";
-        cin >> process[i].burst_time;
+    vector<Process> processes(p);
+
+    freopen("input.txt", "r", stdin);
+    for (auto & process : processes) {
+        cin >> process.name;
+        cin >> process.burst_time;
     }
 
     for (int i = 0; i < p; ++i) {
         if(i == 0) {
-            process[i].completion_time = process[i].burst_time;
+            processes[i].completion_time = processes[i].burst_time;
         }
         else{
-            process[i].completion_time = process[i-1].completion_time + process[i].burst_time;
+            processes[i].completion_time = processes[i-1].completion_time + processes[i].burst_time;
         }
     }
 
-//    cout<<"Grant Chart:"<<endl;
-//    for (int i = 0; i < p; ++i) {
-//        cout<<" | "<<process[i].name;
-//    }
-//    cout<<" |" << endl;
-//
-//    for (int i = 0; i < p; ++i) {
-//        if(i ==0) {
-//            cout <<" 0 "<<"   "<<process[i].completion_time;
-//            cout<<"  ";
-//        } else{
-//            cout<<"  "<<process[i].completion_time<<"  ";
-//        }
-//    }
-
-    int previous_time=0;
+    cout<<"Grant Chart:"<<endl;
     for (int i = 0; i < p; ++i) {
-        cout<<"Execution Time: ("<<previous_time<<","<<process[i].completion_time<<") --->"<<" Process: "<<process[i].name<<endl;
-        previous_time = process[i].completion_time;
+        cout<<" | "<<processes[i].name;
+    }
+    cout<<" |" << endl;
+
+    for (int i = 0; i < p; ++i) {
+        if(i ==0) {
+            cout <<" 0 "<<"   "<<processes[i].completion_time;
+            cout<<"  ";
+        } else{
+            cout<<"  "<<processes[i].completion_time<<"  ";
+        }
     }
 
+//    cout<<"Gantt Chart: "<<endl;
+//    int previous_time=0;
+//    for (int i = 0; i < p; ++i) {
+//        cout<<"Execution Time: ("<<previous_time<<","<<processes[i].completion_time<<") --->"<<" Process: "<<processes[i].name<<endl;
+//        previous_time = processes[i].completion_time;
+//    }
+
 
     for (int i = 0; i < p; ++i) {
-        process[i].turn_around_time=(process[i].completion_time-process[i].arrival_time);
-        process[i].waiting_time = (process[i].completion_time-process[i].burst_time);
+        processes[i].turn_around_time = (processes[i].completion_time - processes[i].arrival_time);
+        processes[i].waiting_time = (processes[i].completion_time - processes[i].burst_time);
+    }
+
+    cout<<endl<<endl;
+    cout << "Completion Time: "<<endl;
+    for (auto & process: processes) {
+        cout<<process.name << " = " << process.completion_time << endl;
     }
 
     cout<<endl<<"Turn Around Time: "<<endl;
     for (int i = 0; i < p; ++i) {
-        cout<<process[i].name<<"="<<process[i].turn_around_time<<endl;
+        cout<<processes[i].name<<" = "<<processes[i].turn_around_time<<endl;
     }
 
-    cout<<"Waiting Time: "<<endl;
+    cout<<endl<<"Waiting Time: "<<endl;
     for (int i = 0; i < p; ++i) {
-        cout<<process[i].name<<"="<<process[i].waiting_time<<endl;
+        cout<<processes[i].name<<" = "<<processes[i].waiting_time<<endl;
     }
+
+    cout<< endl;
 
     double sum_turn_around_time=0;
     for (int i = 0; i < p; ++i) {
-        sum_turn_around_time +=process[i].turn_around_time;
+        sum_turn_around_time += processes[i].turn_around_time;
     }
-    cout<<"Average TurnAround Time: "<<(sum_turn_around_time/p)<<endl;
+    cout<<"Average TurnAround Time: "<<(sum_turn_around_time / p)<<endl;
 
     double sum_waiting_time=0;
     for (int i = 0; i < p; ++i) {
-        sum_waiting_time +=process[i].waiting_time;
+        sum_waiting_time += processes[i].waiting_time;
     }
-    cout<<"Average Waiting Time: "<<(sum_waiting_time/p);
+    cout<<"Average Waiting Time: "<<(sum_waiting_time / p) << endl;
 
     return 0;
 }
